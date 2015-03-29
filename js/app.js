a57bd503b6d12aa37d269861a08493415a3aa8b1
leoreq <leoreq@gmail.com>
2015-03-29 01:51:20 -0400

    finished app: distance check, distance comparrison, publising of results, and ammendment on coocking

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@
 var secretNumber=0;
 var guessNumber=0;
 var guessCount=0;
+var currentDistance=0;
+var oldDistance=0;
+
 $(document).ready(function(){
-	
+
+	newGame();// Start with a new game once page loads
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -25,9 +30,10 @@ $(document).ready(function(){
   	{
   		guessCount=0;
   		$('#count').text(guessCount);
+  		$('#feedback').text('Make your Guess!');
   		$("#guessList li").remove();
-  		var secretNumber=Math.floor((Math.random() * 10) + 1);
-  		console.log(secretNumber);
+  		secretNumber=Math.floor((Math.random() * 100) + 1);
+  		console.log("The secret number is: "+secretNumber);
   	}
   	
 	/*--- Store the guess if he presses the enter button after typing  ---*/
@@ -59,16 +65,79 @@ $(document).ready(function(){
 	{
 		if (num==NaN || num%1!=0) 
 			{
-	    		alert("please enter an integer");
+	    		alert("Please enter an integer");
+			}
+		else if (num<0 || num>100) 
+			{
+	    		alert("Please enter a number between 1 and 100");
 			}
 		else
 			{
 				guessCount++;
 				console.log("Guess count is " +guessCount);
 				$('#count').text(guessCount);
+				currentDistance=Math.abs(num-secretNumber);
 				console.log("DEBUG:"+"<li> Try # :"+guessCount+" guess:"+num+" </li>");
-				$("#guessList").append("<li> "+num+" </li>"); // append your element to the #console element
+				$("#guessList").append("<li> "+num+checkDistance(currentDistance)+" </li>"); // append your element to the #console element
+				$('#feedback').text(compareDistance(currentDistance));
 			}	
+	}
+
+/*------------This function will evaluate distance, and print the message based on proximity-------------*/
+	function checkDistance (distance)
+	{
+		if (distance==0) 
+		{
+			return(": BINGO!!!!")
+		}
+		else if (distance<=10) 
+		{
+			return(": VERY HOT")
+		}
+		else if (distance<=20) 
+		{
+			return(": HOT")
+		}
+		else if (distance<=30) 
+		{
+			return(": WARM")
+		}
+		else if (distance<=50) 
+		{
+			return(": COLD")
+		}
+		else
+		{
+			return(": ICE COLD")
+		}
+	}
+
+	/*------------This function will compare current distance with old distance, and advice on how better off the guess is-------------*/
+	function compareDistance (distance)
+	{
+		console.log("DEBUG: Comparing old "+oldDistance+" with new "+distance);
+
+		if (guessCount<=1) //If its the first guess, no comparrison comment will be made.
+		{
+			oldDistance=distance;
+			return(" FIRST SHOT ");
+		}
+		else if (oldDistance==distance)
+		{
+			oldDistance=distance;
+			return(" SAME ");
+		}
+		else if (oldDistance>distance)
+		{
+			oldDistance=distance;
+			return(" HOTTER ");	
+
+		}
+		else 
+		{
+			oldDistance=distance;
+			return(" COLDER ");	
+		}
 	}
 
 });
